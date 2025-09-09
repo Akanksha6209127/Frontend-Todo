6c7917ff471ae871031757c56b88a5350813d445
Akanksha6209127 <akanksha290803@gmail.com>
2025-09-09 16:28:18 +0530

    Change the background of popup box.

--- src/components/todo/CreateBoxDialog.tsx
+++ src/components/todo/CreateBoxDialog.tsx
@@ -33,18 +33,13 @@ export default function CreateBoxDialog({ onCreate }: Props) {
       <DialogTrigger asChild>
         <Button className="bg-blue-600 text-white hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600">Create</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-lg shadow-lg">
         <DialogHeader>
-          {/* <DialogTitle className="text-lg font-semibold">Create New {type === "todo" ? "Todo" : "List"}</DialogTitle> */}
-          <DialogTitle
-            className="text-lg font-semibold text-gray-900 dark:text-gray-100"
-          >
+          <DialogTitle className="text-lg font-semibold text-gray-900 dark:text-gray-100">
             Create New {type === "todo" ? "Todo" : "List"}
           </DialogTitle>
-
         </DialogHeader>
-      
-      
+
         <div className="flex gap-4 my-2">
           <label className="flex items-center gap-2 cursor-pointer text-gray-700 dark:text-gray-300">
             <input
@@ -89,3 +84,4 @@ export default function CreateBoxDialog({ onCreate }: Props) {
     </Dialog>
   );
 }
+    
